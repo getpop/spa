b133d0808674b3452e6cf31d50490dda65ea0ff2
Leo <losoviz@gmail.com>
2019-05-29 11:02:55 +0800

    Enabled default parameters in services.yaml

--- src/Component.php
+++ src/Component.php
@@ -1,13 +1,14 @@
 <?php
 namespace PoP\SPA;
 
+use PoP\Root\Component\AbstractComponent;
 use PoP\Root\Component\YAMLServicesTrait;
 use PoP\Root\Component\PHPConfigurableServicesTrait;
 
 /**
  * Class required to check if this component exists and is active
  */
-class Component
+class Component extends AbstractComponent
 {
     use YAMLServicesTrait;
     use PHPConfigurableServicesTrait;
@@ -17,7 +18,17 @@ class Component
      */
     public static function init()
     {
+        parent::init();
         self::initYAMLServices(dirname(__DIR__));
+    }
+
+    /**
+     * Initialize Service Configuration
+     *
+     * @return void
+     */
+    public static function boot()
+    {
         self::initPHPServiceConfiguration(dirname(__DIR__));
     }
 }
