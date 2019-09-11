edbe16ae2328ae836d525b91defcd7cb397c889c
Leo <losoviz@gmail.com>
2019-09-11 11:27:42 +0800

    Commented version

--- src/Component.php
+++ src/Component.php
@@ -11,7 +11,7 @@ use PoP\SPA\Config\ServiceConfiguration;
 class Component extends AbstractComponent
 {
     use YAMLServicesTrait;
-    const VERSION = '0.1.0';
+    // const VERSION = '0.1.0';
 
     /**
      * Initialize services
